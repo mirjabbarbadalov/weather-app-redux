30a37aa916c4aa2e57b69a6fc0d447755b3c84c3
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-08-09 18:05:36 +0400

    API_KEY changed

--- src/store/actions/weatherActions.js
+++ src/store/actions/weatherActions.js
@@ -2,6 +2,8 @@ const FETCH_WEATHER_REQUEST = "FETCH_WEATHER_REQUEST";
 const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS";
 const FETCH_WEATHER_FAILURE = "FETCH_WEATHER_FAILURE";
 
+import API_KEY from "../../apikey";
+
 export const fetchPosts = (location) => {
   return async (dispatch) => {
     dispatch({
@@ -10,7 +12,7 @@ export const fetchPosts = (location) => {
 
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?appid=fe0fbe3c79ea276dc2211dce2a13f066&q=${location}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&q=${location}&units=metric`
       );
       const data = await response.json();
       // console.log(data);
